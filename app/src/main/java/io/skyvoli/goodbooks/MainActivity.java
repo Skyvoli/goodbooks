d4f9ea1b140588250000df9971d9c2362dc9ae7a
Michael Tobolla <m.tobolla@t-online.de>
2024-04-17 11:21:33 +0200

    Leave input fields on click outside

--- app/src/main/java/io/skyvoli/goodbooks/MainActivity.java
+++ app/src/main/java/io/skyvoli/goodbooks/MainActivity.java
@@ -1,6 +1,12 @@
 package io.skyvoli.goodbooks;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -55,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
             books.forEach((book -> book.setCover(fileStorage.getImage(book.getIsbn()))));
             globalViewModel.setBooksAsynchronous(books);
         }).start();
-
     }
 
     @Override
@@ -63,5 +68,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
