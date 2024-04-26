02c1d7fb6a266411d3629a1d76c8de3ff5509b42
Michael Tobolla <m.tobolla@t-online.de>
2024-04-26 06:44:07 +0200

    Migrate existing data

--- app/src/main/java/io/skyvoli/goodbooks/ui/fragments/home/HomeFragment.java
+++ app/src/main/java/io/skyvoli/goodbooks/ui/fragments/home/HomeFragment.java
@@ -1,16 +1,24 @@
 package io.skyvoli.goodbooks.ui.fragments.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.room.Room;
 
 import io.skyvoli.goodbooks.databinding.FragmentHomeBinding;
+import io.skyvoli.goodbooks.model.GlobalViewModel;
+import io.skyvoli.goodbooks.storage.database.AppDatabase;
+import io.skyvoli.goodbooks.web.BookResolver;
 
 public class HomeFragment extends Fragment {
 
@@ -20,12 +28,32 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
+        GlobalViewModel globalViewModel = new ViewModelProvider(requireActivity()).get(GlobalViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
+        final Button magicButton = binding.button;
+        final ProgressBar progressCircular = binding.progressCircular;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        magicButton.setOnClickListener(v -> {
+            progressCircular.setVisibility(View.VISIBLE);
+            Context context = requireContext();
+            new Thread(() -> {
+
+                AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, "books")
+                        .build();
+                BookResolver resolver = new BookResolver();
+                globalViewModel.getBooks().forEach(book -> db.bookDao().update(resolver.resolveBook(book.getIsbn(), 20)));
+
+                if (isAdded()) {
+                    requireActivity().runOnUiThread(() -> progressCircular.setVisibility(View.GONE));
+                }
+                Log.d("", "Resolved");
+            }).start();
+        });
 
         return root;
     }
