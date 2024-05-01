9a47be8c485720b5f22abd1f3eedb3e4ee40e952
Michael Tobolla <m.tobolla@t-online.de>
2024-05-01 19:06:10 +0200

    Added schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,13 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["room.schemaLocation"] =
+                    "$projectDir/schemas"
+            }
+        }
     }
 
     buildTypes {
