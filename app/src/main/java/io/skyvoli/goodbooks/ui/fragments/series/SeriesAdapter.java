f222644161a7d2fca6f37a67cc6cedf0daa50e85
Michael Tobolla <m.tobolla@t-online.de>
2024-05-02 08:07:38 +0200

    Started new fragment_series

---
+++ app/src/main/java/io/skyvoli/goodbooks/ui/fragments/series/SeriesAdapter.java
@@ -0,0 +1,61 @@
+package io.skyvoli.goodbooks.ui.fragments.series;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import io.skyvoli.goodbooks.databinding.FragmentSeriesBinding;
+import io.skyvoli.goodbooks.ui.fragments.series.PlaceholderContent.PlaceholderItem;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class SeriesAdapter extends RecyclerView.Adapter<SeriesAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public SeriesAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentSeriesBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentSeriesBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
