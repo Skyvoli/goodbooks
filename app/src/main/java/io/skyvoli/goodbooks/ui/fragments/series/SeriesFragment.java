f2472ed007471c69d95707ff1bce6f4b13100358
Michael Tobolla <m.tobolla@t-online.de>
2024-05-02 12:58:44 +0200

    Filter

--- app/src/main/java/io/skyvoli/goodbooks/ui/fragments/series/SeriesFragment.java
+++ app/src/main/java/io/skyvoli/goodbooks/ui/fragments/series/SeriesFragment.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import io.skyvoli.goodbooks.databinding.FragmentSeriesBinding;
 import io.skyvoli.goodbooks.global.GlobalController;
@@ -33,7 +35,16 @@ public class SeriesFragment extends Fragment {
         View root = binding.getRoot();
 
         GlobalController globalController = new GlobalController(requireActivity());
-        List<Book> series = globalController.getBooks();
+        List<Book> books = globalController.getBooks();
+
+        Set<String> titles = books.stream().map(Book::getTitle).collect(Collectors.toSet());
+
+        List<Series> series = titles.stream().map(s ->
+                        new Series(s, books.stream()
+                                .filter(book -> s.equalsIgnoreCase(book.getTitle()))
+                                .findFirst().get().getNullableCover()))
+                .collect(Collectors.toList());
+
 
         RecyclerView recyclerView = binding.series;
         recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 3));
