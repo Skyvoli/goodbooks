51b79aa5fda84b28060739bcec0bc955cec02e78
Michael Tobolla <m.tobolla@t-online.de>
2024-05-05 11:19:06 +0200

    Connected series and books

--- app/src/main/java/io/skyvoli/goodbooks/storage/database/AppDatabase.java
+++ app/src/main/java/io/skyvoli/goodbooks/storage/database/AppDatabase.java
@@ -11,7 +11,7 @@ import io.skyvoli.goodbooks.storage.database.dao.SeriesDao;
 import io.skyvoli.goodbooks.storage.database.dto.Book;
 import io.skyvoli.goodbooks.storage.database.dto.Series;
 
-@Database(entities = {Book.class, Series.class}, version = 3)
+@Database(entities = {Book.class, Series.class}, version = 4)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract BookDao bookDao();
 
@@ -29,6 +29,32 @@ public abstract class AppDatabase extends RoomDatabase {
         public void migrate(@NonNull SupportSQLiteDatabase database) {
             database.execSQL("CREATE TABLE IF NOT EXISTS series (seriesId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
                     "title TEXT, author TEXT)");
+        }
+    };
+
+    public static final Migration MIGRATION_3_4 = new Migration(3, 4) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("DROP TABLE series");
+            database.execSQL("CREATE TABLE IF NOT EXISTS series (seriesId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    "title TEXT, author TEXT)");
+            database.execSQL("INSERT INTO series (title, author) SELECT title, author FROM books GROUP BY title");
+            database.execSQL("ALTER TABLE books ADD COLUMN seriesId INTEGER");
+            database.execSQL("CREATE TABLE IF NOT EXISTS newBooks " +
+                    "(isbn TEXT NOT NULL, " +
+                    "title TEXT, " +
+                    "subtitle TEXT, " +
+                    "part INTEGER, " +
+                    "author TEXT, " +
+                    "resolved INTEGER NOT NULL, " +
+                    "seriesId INTEGER NOT NULL, " +
+                    "PRIMARY KEY(isbn), " +
+                    "FOREIGN KEY(seriesId) REFERENCES series(seriesId) ON UPDATE NO ACTION ON DELETE RESTRICT)");
+            database.execSQL("INSERT INTO newBooks (isbn, title, subtitle, part, author, resolved, seriesId) SELECT isbn, title, subtitle, part, author, resolved, 1 as seriesId FROM books");
+            database.execSQL("DROP TABLE books");
+            database.execSQL("ALTER TABLE newBooks RENAME TO books");
+            database.execSQL("UPDATE books SET seriesId = (SELECT s.seriesId FROM books, series s WHERE books.title LIKE s.title)");
+
         }
     };
 }
