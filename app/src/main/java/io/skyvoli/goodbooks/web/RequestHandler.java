e1ad303d0b1856386d5ac5587f5e4946bd7c8b10
Michael Tobolla <m.tobolla@t-online.de>
2024-08-15 13:18:54 +0200

    Reworked API Interface + Google Request

--- app/src/main/java/io/skyvoli/goodbooks/web/RequestHandler.java
+++ app/src/main/java/io/skyvoli/goodbooks/web/RequestHandler.java
@@ -3,11 +3,15 @@ package io.skyvoli.goodbooks.web;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
@@ -36,6 +40,29 @@ public class RequestHandler {
         return document;
     }
 
+    public Optional<JsonNode> getJsonDocument(String stringUrl, int timeout) {
+        URL url;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            return Optional.empty();
+        }
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Optional<JsonNode>> future = executorService.submit(() -> fetchJson(url));
+
+        Optional<JsonNode> node;
+        try {
+            node = future.get(timeout, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return Optional.empty();
+        } catch (ExecutionException | TimeoutException e) {
+            return Optional.empty();
+        }
+        return node;
+    }
+
     private Optional<Document> fetchDocument(String url) {
         Document doc;
         try {
@@ -46,6 +73,17 @@ public class RequestHandler {
             return Optional.empty();
         }
         return Optional.ofNullable(doc);
+    }
+
+    private Optional<JsonNode> fetchJson(URL url) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node;
+        try {
+            node = mapper.readTree(url);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+        return Optional.of(node);
     }
 
 
--- app/src/main/java/io/skyvoli/goodbooks/web/api/BookApi.java
+++ app/src/main/java/io/skyvoli/goodbooks/web/api/BookApi.java
@@ -2,15 +2,13 @@ package io.skyvoli.goodbooks.web.api;
 
 import android.graphics.drawable.Drawable;
 
-import org.jsoup.nodes.Document;
+import java.util.Optional;
 
 import io.skyvoli.goodbooks.storage.database.dto.Book;
 
 public interface BookApi {
 
-    String buildUrl(String isbn);
-
-    Book serializeDocument(Document document, String isbn, int timeout);
+    Optional<Book> getBook(String isbn, int timeout);
 
     Drawable loadImage(String isbn, int timeout);
 }
